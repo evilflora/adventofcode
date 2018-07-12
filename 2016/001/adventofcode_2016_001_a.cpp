d133ade924894a4d07678d4fbf86a5e14e9109b6
evilflora <39695070+evilflora@users.noreply.github.com>
2018-07-12 11:30:13 +0200

    Update adventofcode_2016_001_a.cpp

--- 2016/001/adventofcode_2016_001_a.cpp
+++ 2016/001/adventofcode_2016_001_a.cpp
@@ -13,7 +13,6 @@ int main() {
   
   string input = "R3,L5,R1,R2,L5,R2,R3,L2,L5,R5,L4,L3,R5,L1,R3,R4,R1,L3,R3,L2,L5,L2,R4,R5,R5,L4,L3,L3,R4,R4,R5,L5,L3,R2,R2,L3,L4,L5,R1,R3,L3,R2,L3,R5,L194,L2,L5,R2,R1,R1,L1,L5,L4,R4,R2,R2,L4,L1,R2,R53,R3,L5,R72,R2,L5,R3,L4,R187,L4,L5,L2,R1,R3,R5,L4,L4,R2,R5,L5,L4,L3,R5,L2,R1,R1,R4,L1,R2,L3,R5,L4,R2,L3,R1,L4,R4,L1,L2,R3,L1,L1,R4,R3,L4,R2,R5,L2,L3,L3,L1,R3,R5,R2,R3,R1,R2,L1,L4,L5,L2,R4,R5,L2,R4,R4,L3,R2,R1,L4,R3,L3,L4,L3,L1,R3,L2,R2,L4,L4,L5,R3,R5,R3,L2,R5,L2,L1,L5,L1,R2,R4,L5,R2,L4,L5,L4,L5,L2,L5,L4,R5,R3,R2,R2,L3,R3,L2,L5"; // chaine d entree
   //string input = "R3,L5,R1,R2,L5,R2,R3,L2,L5,R5"; // chaine d entree
-  char boussole[4] = {'N','E','S','O'};
   short direction = 0; // voir switch
   vector<string> tab{explode(input, ',')}; // on decoupe la chaine en une liste de commandes
   int x = 0, y = 0; // point de depart
