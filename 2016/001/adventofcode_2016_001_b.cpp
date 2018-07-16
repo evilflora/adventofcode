472bdd7f466bfd67d2993f0c963147f974f10c74
evilflora <39695070+evilflora@users.noreply.github.com>
2018-07-16 10:46:16 +0200

    Create adventofcode_2016_001_b.cpp

---
+++ 2016/001/adventofcode_2016_001_b.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <array>
+#include <map>
+
+#define MAP map<short,map<short, short>>
+
+using namespace std;
+
+const vector<string> explode(const string&, const char&);
+const int get_weight(const string&);
+
+int main() {
+  clock_t begin = clock();
+  
+  string input = "R3,L5,R1,R2,L5,R2,R3,L2,L5,R5,L4,L3,R5,L1,R3,R4,R1,L3,R3,L2,L5,L2,R4,R5,R5,L4,L3,L3,R4,R4,R5,L5,L3,R2,R2,L3,L4,L5,R1,R3,L3,R2,L3,R5,L194,L2,L5,R2,R1,R1,L1,L5,L4,R4,R2,R2,L4,L1,R2,R53,R3,L5,R72,R2,L5,R3,L4,R187,L4,L5,L2,R1,R3,R5,L4,L4,R2,R5,L5,L4,L3,R5,L2,R1,R1,R4,L1,R2,L3,R5,L4,R2,L3,R1,L4,R4,L1,L2,R3,L1,L1,R4,R3,L4,R2,R5,L2,L3,L3,L1,R3,R5,R2,R3,R1,R2,L1,L4,L5,L2,R4,R5,L2,R4,R4,L3,R2,R1,L4,R3,L3,L4,L3,L1,R3,L2,R2,L4,L4,L5,R3,R5,R3,L2,R5,L2,L1,L5,L1,R2,R4,L5,R2,L4,L5,L4,L5,L2,L5,L4,R5,R3,R2,R2,L3,R3,L2,L5"; // chaine d entree
+  //string input = "R8, R4, R4, R8"; // chaine d entree
+  short direction = 0; // voir switch
+  bool stop = false;
+  MAP memory;
+  vector<string> tab{explode(input, ',')}; // on decoupe la chaine en une liste de commandes
+  int x = 0, y = 0, i = 0; // point de depart
+  short len = tab.size(); // le nombres de commandes à executer
+  
+  for(int i = 0; i < len; i++) { // transformer en while
+   short len = 0;
+   direction = (direction + (tab[i][0] == 'R' ? 1 : 3) ) % 4;
+   switch(direction) {
+    case 0: // Nord
+      len = y + get_weight(tab[i]);
+      for(; y < len; y++) if(++memory[x][y] > 1) break;
+    break;
+    case 1: // Est
+      len = x + get_weight(tab[i]);
+      for(; x < len; x++) if(++memory[x][y] > 1) break;
+    break;
+    case 2: // Sud
+      len = y - get_weight(tab[i]);
+      for(; y > len; y--) if(++memory[x][y] > 1) break;
+    break;
+    case 3: // Ouest
+      len = x - get_weight(tab[i]);
+      for(; x > len; x--) if(++memory[x][y] > 1) break;
+    break;
+   }
+  } // listage des commandes
+	
+  clock_t end = clock();
+  printf("Distance %d\n",abs(x) + abs(y)); // 182
+  printf("Duration = %f s\n",(double)(end - begin) / CLOCKS_PER_SEC);
+}
+
+const vector<string> explode(const string& s, const char& c) {
+	string buff{""};
+	vector<string> v;
+	
+	for(auto n:s)
+	{
+		if(n != c && n != ' ') buff+=n; else
+		if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+	}
+	if(buff != "") v.push_back(buff);
+	
+	return v;
+}
+
+const int get_weight(const string& s) {
+  string buff = "0";
+  
+  for(auto n:s) {
+    if(n >= '0' && n <= '9') { buff+=n; }
+  } 
+    
+  return stoi(buff);
+}
